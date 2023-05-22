0d7a412534c81a34132c8f2fbbbd6bbc9d40af45
Breno Angelo <brenoangeloal@gmail.com>
2023-05-21 23:10:58 -0300

    fix: Cards SEction types

--- src/components/CardsSection.tsx
+++ src/components/CardsSection.tsx
@@ -1,13 +1,22 @@
 import { Card } from './Card';
 
-export function CardsSection({characters}) {
+interface CardsSectionProps {
+  characters: {
+    image: string;
+    name: string;
+    status: 'Alive' | 'Dead';
+    id: number;
+  }[];
+}
+
+export function CardsSection({ characters }: CardsSectionProps) {
   return (
     <main className="container flex flex-wrap gap-16 items-center mt-20">
       {characters.map((card) => (
         <Card
           image={card.image}
           name={card.name}
-          status={card.status as 'Alive' | 'Dead'}
+          status={card.status}
           id={card.id}
         />
       ))}
